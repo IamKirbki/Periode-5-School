4c9f05f91a1ac2fab3e2f82c354e7902cfbcb741
IamKirbki <134633469+IamKirbki@users.noreply.github.com>
2024-04-11 09:40:54 +0200

    Small fix

--- websites/pokedex-front/src/components/Battle.vue
+++ websites/pokedex-front/src/components/Battle.vue
@@ -287,6 +287,7 @@ export default {
         end(){
             setTimeout(() => {
                 localStorage.removeItem("opponent")
+                this.router.push('/chatroom')
             }, 5000)
         }
     }
